afeb8b3290cbac8efea5f3c5cc52a7fef6382a98
Merve Gök <merve_gok@epam.com>
2025-06-09 03:05:16 +0300

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventoryManager.API.Data;
+using InventoryManager.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly InventoryContext _context;
+
+        public ProductsController(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _context.Products.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAll), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Product updated)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            product.Name = updated.Name;
+            product.Description = updated.Description;
+            product.Price = updated.Price;
+            product.Quantity = updated.Quantity;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Data/InventoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InventoryManager.API.Models;
+using System.Collections.Generic;
+
+
+namespace InventoryManager.API.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
